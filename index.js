c5a316a8c9bc14c90a603460128d0afba18387a8
mussaimo <okemwamoses@gmail.com>
2016-10-06 17:30:30 +0300

    POST listener on hello route

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var bodyParser = require('body=parser');
+var hellobot = require('./hellobot');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 // test route
-app.get('/', function (req, res) { res.status(200).send('Hello world') });
-
+app.get('/', function (req, res) { 
+    res.status(200).send('Hello world') 
+});
 
 // error handler
 app.use(function (err, req, res, next){
@@ -22,3 +22,6 @@ app.use(function (err, req, res, next){
 app.listen(port, function() {
     console.log('Slack bot listening on port ' + port)
 });
+
+// route that listens for a POST to /hello.
+app.post('/hello', hellobot);
