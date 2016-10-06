f4a9c38d92b3476823a42bcc42dffe8fc7b2bf72
mussaimo <okemwamoses@gmail.com>
2016-10-06 17:18:50 +0300

    main executable file

--- index.js
+++ index.js
@@ -9,5 +9,16 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-// test code
+// test route
 app.get('/', function (req, res) { res.status(200).send('Hello world') });
+
+
+// error handler
+app.use(function (err, req, res, next){
+    console.error(err.stack);
+    res.status(400).send(err.message);
+});
+
+app.listen(port, function() {
+    console.log('Slack bot listening on port ' + port)
+});
