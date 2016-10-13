193956a0a0a5c720b4eadbf3ed270375e1c97cdf
mussaimo <okemwamoses@gmail.com>
2016-10-13 16:55:07 +0300

    *21. - currently posting user text channel      - posting uniques user ID to channel instead of username      - Works locally(needs to be oushed to heroku)

--- index.js
+++ index.js
@@ -30,3 +30,77 @@ app.post('/hello', hellobot);
 
 // app.post('/bot', bot);
 
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//      ███████╗██╗      █████╗  ██████╗██╗  ██╗    ██████╗     ██████╗  ████████╗      //
+//      ██╔════╝██║     ██╔══██╗██╔════╝██║ ██╔╝    ██║  ██╗   ██║   ██║    ██╔══╝      //
+//      ███████╗██║     ███████║██║     █████╔╝     ██████═╝   ██║   ██║    ██║         //
+//      ╚════██║██║     ██╔══██║██║     ██╔═██╗     ██║  ██╗   ██║   ██║    ██║         //
+//      ███████║███████╗██║  ██║╚██████╗██║  ██╗    ██████═╝    ██████╔╝    ██║         //
+//      ╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝    ╚═════╝     ╚═════╝     ╚═╝         //
+//                   11-10-16 | mussaimo | okemwamoses@gmail.com                        //
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Requiring our module
+var slackAPI = require('slackbotapi');
+
+// Starting
+var slack = new slackAPI({
+    'token': 'xoxb-89845683031-7xwrpgaTzy7PdyNBF5YHKwT8',
+    'logging': true,
+    'autoReconnect': true
+});
+
+// Slack on EVENT message, send data
+slack.on('message', function (data) {
+
+    // functions global scope
+    var userName = data.user;   
+
+
+    // If no text, return.
+    if (typeof data.text === 'undefined') return;
+    // If someone says `help` respond to their message with 'user OOH, CAKE!! :cake:'
+    if (data.text === 'help!' && data.user !== 'slackbot') {
+        
+        slack.sendMsg(data.channel, '@' + slack.getUser(data.user).name + ' how may I help you! & \n have you tried this? \n wdnvsndjnfj \n jdfnjidnfjin \n owhufhuiwhdgufh');
+
+    } else if (data.text === 'yes' || 'Yes' && data.text !== 'help' && data.user !== 'incoming-webhook' && data.user !== 'houdinni') {
+
+        slack.sendMsg(data.channel, 'What is your query my friend');
+
+        // var userHelp = data.text;
+        // var arr = userHelp.split(' ');
+        // var custom = 'how do you host on heroku?';   
+        // var arrCustom = custom.split(',');
+        // var helpDesk = 'help-desk';
+
+
+        console.log('before help coming ' + data.channel + ' ' + userHelp);
+
+        if (data.text.length >= 10){
+
+            slack.sendMsg(data.channel, 'help is coming soon');
+            console.log('after help coming ' + data.channel + ' ' + userHelp);
+
+
+            var helpDesk = 'stat';
+            var userHelp = data.text;
+
+            console.log(userName);
+
+
+
+            var IncomingWebhook = require('@slack/client').IncomingWebhook;
+            var url = process.env.SLACK_WEBHOOK_URL || 'https://hooks.slack.com/services/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';
+            var wh = new IncomingWebhook(url);
+            
+            wh.send('@' + userName + ' asked ' + userHelp);
+        } 
+    } else{
+
+    }
+});
